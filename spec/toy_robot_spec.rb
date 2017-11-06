ace2a56c861e0db28a198596f400c63d94133e4a
Josephine Wright <jozright@gmail.com>
2017-11-06 15:49:00 +0100

    Add commands and tests for robot

--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -1,14 +1,183 @@
 require "./toy_robot"
 
 describe ToyRobot do
-  let(:x) { "0" }
-  let(:y) { "2" }
+  let(:toy_robot) { ToyRobot.new }
+  let(:x) { 2 }
+  let(:y) { 2 }
   let(:direction) { "NORTH" }
-  let(:toy_robot) { ToyRobot.new(x, y, direction) }
 
-  it "initializes with coordinates and a direction" do
-    expect(toy_robot.x).to eq(x)
-    expect(toy_robot.y).to eq(y)
-    expect(toy_robot.direction).to eq(direction)
+  it "initializes" do
+  end
+
+  describe "place!" do
+    before { toy_robot.place!(x, y, direction) }
+
+    it "sets the position and direction" do
+      expect(toy_robot.x).to eq(x)
+      expect(toy_robot.y).to eq(y)
+      expect(toy_robot.direction).to eq(direction)
+    end
+  end
+
+  describe "move!" do
+    before { toy_robot.place!(x, y, direction) }
+
+    context "direction is NORTH" do
+      let(:direction) { "NORTH" }
+
+      it "increases y by 1" do
+        toy_robot.move!
+        expect(toy_robot.y).to eq(y+1)
+      end
+    end
+
+    context "direction is SOUTH" do
+      let(:direction) { "SOUTH" }
+
+      it "decreases the y by 1" do
+        toy_robot.move!
+        expect(toy_robot.y).to eq(y-1)
+      end
+    end
+
+    context "direction is EAST" do
+      let(:direction) { "EAST" }
+
+      it "increases x by 1" do
+        toy_robot.move!
+        expect(toy_robot.x).to eq(x+1)
+      end
+    end
+
+    context "direction is WEST" do
+      let(:direction) { "WEST" }
+
+      it "decreases the x by 1" do
+        toy_robot.move!
+        expect(toy_robot.x).to eq(x-1)
+      end
+    end
+  end
+
+  describe "left!" do
+    before { toy_robot.place!(x, y, direction) }
+
+    context "direction is NORTH" do
+      let(:direction) { "NORTH" }
+
+      it "turns WEST" do
+        toy_robot.left!
+        expect(toy_robot.direction).to eq("WEST")
+      end
+    end
+
+    context "direction is SOUTH" do
+      let(:direction) { "SOUTH" }
+
+      it "turns EAST" do
+        toy_robot.left!
+        expect(toy_robot.direction).to eq("EAST")
+      end
+    end
+
+    context "direction is EAST" do
+      let(:direction) { "EAST" }
+
+      it "turns NORTH" do
+        toy_robot.left!
+        expect(toy_robot.direction).to eq("NORTH")
+      end
+    end
+
+    context "direction is WEST" do
+      let(:direction) { "WEST" }
+
+      it "turns SOUTH" do
+        toy_robot.left!
+        expect(toy_robot.direction).to eq("SOUTH")
+      end
+    end
+  end
+
+  describe "right!" do
+    before { toy_robot.place!(x, y, direction) }
+
+    context "direction is NORTH" do
+      let(:direction) { "NORTH" }
+
+      it "turns EAST" do
+        toy_robot.right!
+        expect(toy_robot.direction).to eq("EAST")
+      end
+    end
+
+    context "direction is SOUTH" do
+      let(:direction) { "SOUTH" }
+
+      it "turns WEST" do
+        toy_robot.right!
+        expect(toy_robot.direction).to eq("WEST")
+      end
+    end
+
+    context "direction is EAST" do
+      let(:direction) { "EAST" }
+
+      it "turns SOUTH" do
+        toy_robot.right!
+        expect(toy_robot.direction).to eq("SOUTH")
+      end
+    end
+
+    context "direction is WEST" do
+      let(:direction) { "WEST" }
+
+      it "turns NORTH" do
+        toy_robot.right!
+        expect(toy_robot.direction).to eq("NORTH")
+      end
+    end
+  end
+
+  describe "report" do
+    before { toy_robot.place!(x, y, direction) }
+
+    it "outputs the position and direction" do
+      expect(toy_robot.report).to eq("Output: #{x},#{y},#{direction}")
+    end
+  end
+
+  describe "placed?" do
+    context "coordinates and direction are set" do
+      before { toy_robot.place!(x, y, direction) }
+
+      it "returns true" do
+        expect(toy_robot.placed?).to eq(true)
+      end
+    end
+
+    context "x is not set" do
+      before { toy_robot.place!(nil, y, direction) }
+
+      it "returns false" do
+        expect(toy_robot.placed?).to eq(false)
+      end   
+    end
+
+    context "y is not set" do
+      before { toy_robot.place!(x, nil, direction) }
+
+      it "returns false" do
+        expect(toy_robot.placed?).to eq(false)
+      end   
+    end
+
+    context "direction is not set" do
+      before { toy_robot.place!(x, y, nil) }
+
+      it "returns false" do
+        expect(toy_robot.placed?).to eq(false)
+      end   
+    end
   end
 end
--- toy_robot.rb
+++ toy_robot.rb
@@ -1,9 +1,44 @@
 class ToyRobot
   attr_reader :x, :y, :direction
 
-  def initialize(x, y, direction)
+  def place!(x, y, direction)
     @x = x
     @y = y
     @direction = direction
+  end
+
+  def move!
+    case direction
+    when "NORTH" then @y += 1
+    when "SOUTH" then @y -= 1
+    when "EAST"  then @x += 1
+    when "WEST"  then @x -= 1
+    end
+  end
+
+  def left!
+    case direction
+    when "NORTH" then @direction = "WEST"
+    when "SOUTH" then @direction = "EAST"
+    when "EAST"  then @direction = "NORTH"
+    when "WEST"  then @direction = "SOUTH"
+    end
+  end
+
+  def right!
+    case direction
+    when "NORTH" then @direction = "EAST"
+    when "SOUTH" then @direction = "WEST"
+    when "EAST"  then @direction = "SOUTH"
+    when "WEST"  then @direction = "NORTH"
+    end
+  end
+
+  def report
+    "Output: #{x},#{y},#{direction}"
+  end
+
+  def placed?
+    !!(x && y && direction)
   end
 end
